aa618cf3f361e5d64cdba4eeb11d26e2503b70b0
Alamin <alamin.mj.3228@gmail.com>
2021-09-22 11:19:18 +0600

    mouse move event test

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ const pagex = document.querySelectorAll("h2")[0];
 const pagey = document.querySelectorAll("h2")[1];
 console.log(body);
 body.addEventListener("touchmove", (e) => {
-    console.log(e)
+  console.log(e);
   pagex.innerText = e.touches[0].pageX;
   pagey.innerText = e.touches[0].pageY;
-  
+});
+body.addEventListener("contextmenu", (e) => {
+  console.log("contextmenu Click");
+//   e.preventDefaug
 });
